a15c6840e07c68e564815e2ead638e1681439aa6
murod <murad.djanikulov@gmail.com>
2024-03-02 19:27:24 +0500

    1

--- lesson8/src/main/resources/SOLUTION.sql
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-SELECT min(birthday) FROM student;
+SELECT max(birthday) FROM student;
 SELECT min(payment_date) FROM payment;
-SELECT avg(mark) FROM mark WHERE subject_id IN (SELECT id FROM subject WHERE name='Math');
-SELECT min(amount) FROM payment WHERE type_id IN (SELECT id FROM paymenttype WHERE name='WEEKLY');
+select AVG(MARK.MARK) from MARK left join SUBJECT on MARK.SUBJECT_ID = SUBJECT.ID where SUBJECT.NAME='Math';
+select min(PAYMENT.AMOUNT) from PAYMENT left join PAYMENTTYPE on PAYMENT.TYPE_ID = PAYMENTTYPE.ID where PAYMENTTYPE.NAME='WEEKLY';
