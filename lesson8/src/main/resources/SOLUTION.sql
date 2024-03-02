d026e833cb77390af37f065860eaae17e6bbf565
murod <murad.djanikulov@gmail.com>
2024-03-02 17:49:38 +0500

    fixed

--- lesson8/src/main/resources/SOLUTION.sql
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -1,11 +1,11 @@
 -- 1. Select the youngest student's birthday
-SELECT birthday FROM Student ORDER BY birthday DESC LIMIT 1;
+SELECT MAX(birthday) FROM Student;
 
 -- 2. Find the earliest made payment's date
 SELECT MIN(payment_date) FROM Payment;
 
 -- 3. Find the average mark by Math
-SELECT AVG(mark) FROM Mark WHERE subject_id = (SELECT id FROM Subject WHERE name = 'math');
+SELECT AVG(mark) FROM Mark WHERE subject_id = (SELECT id FROM Subject WHERE name = 'Math');
 
 -- 4. Find the min amount of payment for payment type "WEEKLY"
 SELECT MIN(amount) FROM Payment WHERE type_id = (SELECT id FROM PaymentType WHERE name = 'WEEKLY');
